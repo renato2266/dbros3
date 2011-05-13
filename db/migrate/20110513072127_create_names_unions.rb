fd0425a7bbd84bde37ed790b3689e53ab1ea55b4
renato2266 <renato2266@gmail.com>
2011-05-13 09:55:09 +0200

    Upgrade names_unions

--- db/migrate/20110513072127_create_names_unions.rb
+++ db/migrate/20110513072127_create_names_unions.rb
@@ -1,7 +1,13 @@
 class CreateNamesUnions < ActiveRecord::Migration
   def self.up
-  end
-
-  def self.down
+    create_table :names_unions, :id=>false do |t|
+      t.integer :name_id
+      t.integer :union_id
   end
 end
+
+  def self.down
+    drop_table :names_unions
+  end
+end
+
